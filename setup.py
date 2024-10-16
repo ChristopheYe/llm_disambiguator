3b85d2b105db0bdfa24760c69f383997a4eb0136
Ye_Christophe_cye73 <cye73@MITCH-LIN.AD.GATECH.EDU>
2024-10-16 00:53:09 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name="llm_disambiguator",
+    version="0.1.0",
+    description="LLM for named entity disambiguation",
+    # long_description=long_description,
+    url="",
+    author="Anonymous",
+    author_email="",
+    keywords=[
+        "biomedical",
+        "entity",
+        "linking",
+        "entity-linking",
+        "biomedical-entity-linking",
+        "named entity disambiguation",
+    ],
+    packages=find_packages(),
+    python_requires=">= 3.9",
+    install_requires=[
+        "tqdm",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "ujson",
+        "torch",
+        "transformers",
+        "faiss-gpu",
+        "sentence_transformers",
+        "vllm",
+        "openai",
+        "statsmodels",
+    ],
+)
